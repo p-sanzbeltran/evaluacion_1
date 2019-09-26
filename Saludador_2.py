9eeedeeb6465beae9daa527672427763f9c1564f
EEEDU <55740725+EEEDU@users.noreply.github.com>
2019-09-26 09:47:25 +0200

    Add files via upload

---
+++ Saludador_2.py
@@ -0,0 +1,9 @@
+def saludador_2():
+    print"Hola amigo" #instrucciones de salida
+    nombre= raw_input("Como te llamas? ") #instrucciones de entrada
+    print"Bienvenido a python "+ nombre
+    edad= raw_input("Cuantos años tienes? ")
+    print"Muy bien"
+    Antonio= raw_input("Antonio esta muy bueno? ")
+    print"y Antoni es muy guapo"
+saludador_2()
