ea2405441ae12b46fb590e9e246400dff3266d27
EEEDU <55740725+EEEDU@users.noreply.github.com>
2019-10-08 11:57:23 +0200

    Add files via upload

---
+++ primo.py
@@ -0,0 +1,12 @@
+def primo():
+    #i es una variable que toma los valores comprendidos en el rango
+    a=input("Dime un numero: ")
+    for i in range(1,a):
+        if a/i==a:
+            print"El numero es impar"
+        else:
+           print"El numero es par"
+         
+primo()
+
+
