a10b15ddcdfb17a52e318c9be46aa0b71631faea
EEEDU <55740725+EEEDU@users.noreply.github.com>
2019-10-15 11:34:49 +0200

    Add files via upload

---
+++ par_multiplo3.py
@@ -0,0 +1,11 @@
+def par_multiplo3():
+    x=input("Inserte un numero: ")
+    if x%2==0:
+        print "El numero es par"
+    else:
+        print "El numero es impar"
+    if x%3==0:
+        print "El numero es multiplo de tres"
+    else:
+        print "El numnero no es multiplo de tres"
+par_multiplo3()
