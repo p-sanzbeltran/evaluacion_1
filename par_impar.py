b233a039b337f844f4bfefa680f9301c9b2168d8
EEEDU <55740725+EEEDU@users.noreply.github.com>
2019-10-15 12:03:08 +0200

    Update par_impar.py

--- par_impar.py
+++ par_impar.py
@@ -1,5 +1,8 @@
 #queremos que el programa sepa si un numero es par o impar
 def par_impar():
-    a=input("añade un numero: ")
-    
+    a=input("aÃ±ade un numero: ")
+    if a%2==0:
+        print "El numero es par"
+    else:
+        print "El numero es impar"
 par_impar()
